c6cd571c30c6a002dd0f17c64ff27c7f5cfbe70a
MartinVelez2002 <96700377+MartinVelez2002@users.noreply.github.com>
2025-01-23 18:05:56 -0500

    Archivo requirements.txt

--- Gestion/Sprint/models.py
+++ Gestion/Sprint/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Sprint(models.Model):
+    Titulo = models.CharField(max_length=100, unique=True)
+    Fecha_inicio = models.DateField(blank=True, null=False)
+    Fecha_fin = models.DateField(blank=True, null=False)
